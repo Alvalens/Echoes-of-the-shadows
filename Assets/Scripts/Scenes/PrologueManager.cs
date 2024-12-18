9c8869ee77151b061b035534977493a43909cf66
Alvalen <alvalen.shafel04@gmail.com>
2024-12-18 19:10:22 +0700

    feat: add clue about generator

--- Assets/Scripts/Scenes/PrologueManager.cs
+++ Assets/Scripts/Scenes/PrologueManager.cs
@@ -17,12 +17,12 @@ public class PrologueManager : MonoBehaviour
     private EventSystem eventSystem;
 
     private string[] texts = new string[]
-    {
-        "Aku berjalan melewati hutan yang sunyi dan menemukan rumah itu. Rumah tua ini diwariskan oleh kerabat yang hampir tak kuingat. Tugasku hanya <color=yellow>membersihkannya</color>, menyiapkannya untuk dijual, dan meninggalkannya untuk selamanya.",
-        "Ketika aku membuka pintu, bau debu langsung tercium. Lantai berderit di bawah kakiku, dan langkahku bergema di dalam ruangan kosong. Cahaya dari senterku menyinari sudut-sudut gelap, membuat bayangan aneh di dinding.",
+        {
+        "Aku berjalan melewati hutan yang sunyi dan menemukan rumah itu. Rumah tua yang diwariskan oleh kerabat yang hampir tak kuingat. Tugasku hanya <color=yellow>membersihkannya</color>, menyiapkannya untuk dijual, dan meninggalkannya untuk selamanya.",
+        "Ketika aku membuka pintu, bau debu langsung tercium. Cahaya dari senterku menyinari sudut-sudut gelap, membuat bayangan aneh di dinding. Namun, aku menyadari ada masalah lain, <color=yellow>kadang-kadang listrik mati secara tiba-tiba.</color> Di luar rumah, aku menemukan sebuah generator tua. Sepertinya aku harus menggunakannya jika lampu padam.",
         "\"Jangan tinggal sampai malam,\" kata seorang penduduk desa memperingatkan. \"Rumah itu tidak suka tamu, dan mereka bilang <color=yellow>cahaya adalah satu-satunya hal yang bisa membuatnya menjauh.</color>\" Kini, saat bayangan semakin panjang, perasaan tak nyaman mulai muncul.",
         "Aku melihat sebuah jam tanganku, jarumnya berdetak pelan menuju tengah malam. <color=yellow>Jam menunjukkan pukul 03:00 adalah batas waktuku.</color> Aku harus menyelesaikan semuanya sebelum itu, atau mungkin aku tidak akan pernah keluar dari sini dengan selamat."
-    };
+        };
     private int currentTextIndex = 0; // Keeps track of which text to display
 
     void Start()
